42ff248cc75491be2d147bb8cceb2af5000997ce
DziJulia <lorken77@gmail.com>
2024-04-02 20:45:48 +0100

    Exercise 7

--- Exercise 1/Assets/EnemyNavMesh.cs
+++ Exercise 1/Assets/EnemyNavMesh.cs
@@ -1,19 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyNavMesh : MonoBehaviour
 {
+    public Animator animator;
+    public Transform[] patrolPoints;
+    public NavMeshAgent agent;
+    private bool aggro;
+    public float patrolSpeed;
+    public float aggroSpeed;
+    private bool destinationReached;
+    public float destinationThreshold;
+    public float currentVelocity;
     public Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        destinationReached = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<UnityEngine.AI.NavMeshAgent>().destination = player.position;
+        currentVelocity = agent.velocity.magnitude;
+        
+        animator.SetFloat("velocity", currentVelocity);
+
+        if (aggro == true)
+        {
+            agent.destination = player.position;
+        }
+        
+        if (aggro == false && destinationReached == true)
+        {
+            destinationReached = false;
+            agent.speed = patrolSpeed;
+            agent.destination = patrolPoints[Random.Range(0, patrolPoints.Length)].position;
+        }
+        
+        if (Vector3.Distance(transform.position, agent.destination) < destinationThreshold)
+        {
+            destinationReached = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            aggro = true;
+            agent.speed = aggroSpeed;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            agent.destination = player.position;
+            StopCoroutine("AggroTimer");
+            StartCoroutine("AggroTimer");
+        }
+    }
+
+    IEnumerator AggroTimer()
+    {
+        yield return new WaitForSeconds(5);
+        aggro = false;
+        destinationReached = true;
     }
 }
--- Exercise 1/Assets/PlayerNavMesh.cs
+++ Exercise 1/Assets/PlayerNavMesh.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerNavMesh : MonoBehaviour
 {
+    public Animator animator;
+
+    public NavMeshAgent agent;
+
+    public float currentVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentVelocity = agent.velocity.magnitude;
+        
+        animator.SetFloat("velocity", currentVelocity);
+        
         if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay((Input.mousePosition));
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                GetComponent < UnityEngine.AI.NavMeshAgent>().destination = hit.point;
+                agent.destination = hit.point;
             }
         }
         
